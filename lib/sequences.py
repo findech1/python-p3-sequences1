2a040d9fd6debf5d71a4419b1e7addfa70758b7d
Felix <findech6@gmail.com>
2023-11-23 23:49:51 +0300

    completed assignments

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    a, b = 0, 1
+    fibonacci_sequence = []
+
+    for _ in range(n):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+
+    print(fibonacci_sequence)
+
+# Example: Printing the first 10 numbers of the Fibonacci sequence
+print_fibonacci(10)
